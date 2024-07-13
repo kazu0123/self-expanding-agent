d9d1001a9795249c65f9b64be86e362c2f0d108f
kazu0123 <85786535+kazu0123@users.noreply.github.com>
2024-07-14 01:11:31 +0900

    fix function_genration/__init__.py

--- prompts/function_generation/__init__.py
+++ prompts/function_generation/__init__.py
@@ -2,7 +2,10 @@ from pathlib import Path
 from pydantic import Field, BaseModel
 
 from openai.types.chat import ChatCompletionMessageParam
+from openai.types.chat.completion_create_params import Function
 
+
+name = "create_function"
 
 system_prompt = Path("prompts/function_generation/system.md").read_text(encoding="utf-8")
 
@@ -32,7 +35,16 @@ class FunctionInfo(BaseModel):
   description: str = Field(description="関数についての1-2文の短い説明", examples=[
     "現在時刻をISOフォーマットで取得できる関数です。タイムゾーンを指定できます。",
   ])
-  json_schema: str = Field(description="引数のJson Schema", examples=[
+  json_schema: str = Field(description="引数をJsonSchemaで表現してください。JSONをテキスト形式で返却すること。", examples=[
     Path("prompts/function_generation/examples/get_datetime/schema.json").read_text(encoding="utf-8"),
   ])
   files: list[FunctionFile] = Field(description="配置するファイル")
+
+
+functions: list[Function] = [
+  Function(
+    name=name,
+    description="関数を作成し、追加します",
+    parameters=FunctionInfo.model_json_schema(),
+  )
+]
